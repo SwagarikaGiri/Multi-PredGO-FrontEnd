b2ec84d6048683454ec2c5645e95e4241bdbe52f
SwagarikaGiri <swagaarika95@gmail.com>
2020-08-09 00:58:18 +0530

    Updates

---
+++ precache-manifest.abeb25c966ca174472749df44f9e08aa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b5bda0a36ffe8a03ce95bba5db803b1",
+    "url": "/Multi-PredGO-FrontEnd/index.html"
+  },
+  {
+    "revision": "0fcd28cdeef4d96da73e",
+    "url": "/Multi-PredGO-FrontEnd/static/css/main.23211d52.chunk.css"
+  },
+  {
+    "revision": "dcbc686a905231f7b5c3",
+    "url": "/Multi-PredGO-FrontEnd/static/js/2.7ca24509.chunk.js"
+  },
+  {
+    "revision": "0fcd28cdeef4d96da73e",
+    "url": "/Multi-PredGO-FrontEnd/static/js/main.1f9b35c8.chunk.js"
+  },
+  {
+    "revision": "b5143fee327d433f4f56",
+    "url": "/Multi-PredGO-FrontEnd/static/js/runtime~main.0c5a5e50.js"
+  },
+  {
+    "revision": "2f342a353e72c982e4645169b49f54ce",
+    "url": "/Multi-PredGO-FrontEnd/static/media/1.2f342a35.jpeg"
+  },
+  {
+    "revision": "ac862bb813ae9b0048a976cb966b4035",
+    "url": "/Multi-PredGO-FrontEnd/static/media/2.ac862bb8.jpeg"
+  },
+  {
+    "revision": "2ac45e9dfaf52d06423269cfd17b012e",
+    "url": "/Multi-PredGO-FrontEnd/static/media/4.2ac45e9d.jpeg"
+  },
+  {
+    "revision": "a688d468043df0f42c7e4b9a722c2fe2",
+    "url": "/Multi-PredGO-FrontEnd/static/media/header-bg.a688d468.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/Multi-PredGO-FrontEnd/static/media/map-image.f40ed5b1.png"
+  }
+]);
